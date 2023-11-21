85a2f0330006545d7363e958ded0fe00030ba392
aymanrgab <aymanrar2c@gmail.com>
2023-11-21 14:02:12 +0200

    update

--- lineage_kirin710.mk
+++ lineage_kirin710.mk
@@ -6,9 +6,9 @@
 
 ## Common
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 ## Device
 $(call inherit-product, device/huawei/kirin710/device.mk)
